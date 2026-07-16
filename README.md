540734c533ea2f1c625a72e3b6f0d74da708642d
iamaray <arya.nahri@gmail.com>
2026-07-16 09:00:08 -0400

    request parsing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # httpfromtcp
 Following along primeagen's course on building an HTTP server from scratch in Go, as I realized I don't really know how HTTP works.
+
+
+Okay, for the record I know how HTTP works, but do I KNOW how HTTP works?
---
+++ request/request.go
@@ -0,0 +1,79 @@
+package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func (r *RequestLine) ValidHTTP() bool {
+	return r.HttpVersion == "HTTP/1.1"
+}
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+var ErrMalformedStartLine = fmt.Errorf("malformed starting line")
+var ErrIncompleteStartLine = fmt.Errorf("malformed start line")
+var ErrMalformedRequestLine = fmt.Errorf("malformed request line")
+var ErrWrongHttpVersion = fmt.Errorf("HTTP version not supported")
+
+const SEPARATOR = "\r\n"
+
+func parseRequestLine(s string) (*RequestLine, string, error) {
+	idx := strings.Index(s, SEPARATOR)
+	if idx == -1 {
+		return nil, s, nil
+	}
+
+	startLine := s[:idx]
+	restOfMsg := s[idx+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, restOfMsg, ErrMalformedStartLine
+	}
+
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 {
+		return nil, restOfMsg, ErrMalformedRequestLine
+	}
+
+	rl := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   parts[2],
+	}
+	if !rl.ValidHTTP() {
+		return nil, restOfMsg, ErrWrongHttpVersion
+	}
+
+	return rl, restOfMsg, nil
+}
+
+func RequestFromHeader(reader io.Reader) (*Request, error) {
+	var err error
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(
+			fmt.Errorf("unable to io.ReadAll"),
+			err,
+		)
+	}
+
+	str := string(data)
+	rl, _, err := parseRequestLine(str)
+
+	return &Request{
+		RequestLine: *rl,
+	}, err
+}
