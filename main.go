fdcf67e7fe00f63d60d4af5563f6e49d11e13e80
iamaray <arya.nahri@gmail.com>
2026-07-16 17:42:11 -0400

    main refactored

--- main.go
+++ main.go
@@ -3,43 +3,44 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"httpfromtcp/request"
 	"io"
 	"log"
 	"net"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	out := make(chan string, 1)
 
-	go func() {
-		defer f.Close()
-		defer close(out)
+// 	go func() {
+// 		defer f.Close()
+// 		defer close(out)
 
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
+// 		str := ""
+// 		for {
+// 			data := make([]byte, 8)
+// 			n, err := f.Read(data)
+// 			if err != nil {
+// 				break
+// 			}
 
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-			str += string(data)
-		}
+// 			data = data[:n]
+// 			if i := bytes.IndexByte(data, '\n'); i != -1 {
+// 				str += string(data[:i])
+// 				data = data[i+1:]
+// 				out <- str
+// 				str = ""
+// 			}
+// 			str += string(data)
+// 		}
 
-		if len(str) != 0 {
-			out <- str
-		}
-	}()
+// 		if len(str) != 0 {
+// 			out <- str
+// 		}
+// 	}()
 
-	return out
-}
+// 	return out
+// }
 
 func main() {
 	port := ":42069"
@@ -54,8 +55,13 @@ func main() {
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		req, err := request.RequestFromHeader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method:%s\n", req.RequestLine.Method)
+		fmt.Printf("- Target:%s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version:%s\n", req.RequestLine.HttpVersion)
 	}
 }
